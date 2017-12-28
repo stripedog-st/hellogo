cbe0321875db725ae69b2ff19f626241f55f588d
stripedog <33144844+stripedog-st@users.noreply.github.com>
2017-12-28 10:30:25 -0800

    updates

--- hello.go
+++ hello.go
@@ -7,7 +7,7 @@ import(
 )
 
 func indexHandler( w http.ResponseWriter, r *http.Request){
-fmt.Fprintf(w, "Go Node 1. With some changes. I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
+fmt.Fprintf(w, "Go Node 1. This clearly won't go sideways. I'm running on %s with an %s CPU ", runtime.GOOS,runtime.GOARCH)
 
 }
 
